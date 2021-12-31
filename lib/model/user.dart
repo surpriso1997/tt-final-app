634a391a4edd673e167d50a2c0da0d8963b5316b
surpriso1997 <maneesh10chaulagain@gmail.com>
2021-12-31 08:43:37 +0545

    created user model

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -2,6 +2,28 @@ class Useer {
   final String id, email;
   final String? displayName, photoUrl;
 
-  Useer(
-      {required this.id, required this.email, this.displayName, this.photoUrl});
+  Useer({
+    required this.id,
+    required this.email,
+    this.displayName,
+    this.photoUrl,
+  });
+
+  factory Useer.fromJson(Map json) {
+    return Useer(
+      id: json['id'] ?? "",
+      email: json['email'] ?? "",
+      displayName: json['displayName'],
+      photoUrl: json['photoUrl'],
+    );
+  }
+
+  Map toJson() {
+    return {
+      'id': id,
+      'email': email,
+      "displayName": displayName,
+      "photUrl": photoUrl
+    };
+  }
 }
